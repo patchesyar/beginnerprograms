6089cb1a227bb825be5edcfe37ced44a30b75460
John <johnlavoi@gmail.com>
2015-09-28 14:23:51 -0400

    Resolved an issue with the input buffer Hello Name now reads input correctly and consistently

--- beginnerprograms.go
+++ beginnerprograms.go
@@ -7,10 +7,10 @@ func helloWorld(){
 }
 
 func helloName(){
-    var myname string
     fmt.Printf("What is your name?\n")
-    fmt.Scanf("%s", &myname)
-    fmt.Printf("Hello, %s\n",myname)
+    var name string
+    fmt.Scanf("%s", &name)
+    fmt.Printf("Hello, %s", name)
 }
 
 func fizzBuzz(){
@@ -30,7 +30,7 @@ func fizzBuzz(){
 func main(){
     fmt.Println("Select a program\n1.Hello World\n2.Hello Name\n3.FizzBuzz")
     var input int
-    fmt.Scanf("%d",&input)
+    fmt.Scanf("%d\n",&input)
     if input==1{
         helloWorld()
     } else if input==2{
